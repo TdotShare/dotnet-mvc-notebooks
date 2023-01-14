d30c550e8a99021e134d2aa2b01a9f8b237b595a
Jirayu Ch <52649379+TdotShare@users.noreply.github.com>
2023-01-14 16:11:22 +0700

    add action login
    add action login

--- dotnet-mvc-notebooks/Controllers/AuthenController.cs
+++ dotnet-mvc-notebooks/Controllers/AuthenController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using dotnet_mvc_notebooks.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace dotnet_mvc_notebooks.Controllers
 {
@@ -9,6 +10,17 @@ namespace dotnet_mvc_notebooks.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+
+        [HttpPost("auth/login", Name = "auth_login_data")]
+        public IActionResult Login(Login login_data)
+        {
+
+            var data = login_data;
+
+            return Json(data);
+            //return Ok(new { data = login_data });
         }
     }
 }
---
+++ dotnet-mvc-notebooks/Models/Login.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dotnet_mvc_notebooks.Models
+{
+    public class Login
+    {
+        public string? username { get; set; }
+        public string? password { get; set; }
+    }
+}
